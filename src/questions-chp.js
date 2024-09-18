b096760b9b81ca052588a40443aeae22b4874685
nina <ninagbs@abv.bg>
2024-09-18 13:01:15 +0300

    corrected questions

--- src/questions-chp.js
+++ src/questions-chp.js
@@ -215,6 +215,159 @@ const questionsChp = [
             { id: 3, text: "човек – хриле", isCorrect: false },
         ],
     },
+    {
+        text: "Какъв път изминава влак за време t = 2 h при движение със скорост v = 100 h km?",
+        options: [
+            { id: 0, text: "s = 50 km", isCorrect: false },
+            { id: 1, text: "s = 102 km", isCorrect: false },
+            { id: 2, text: "s = 98 km", isCorrect: false },
+            { id: 3, text: "s = 200 km", isCorrect: true },
+        ],
+    },
+    {
+        text: "Килограмът е единица за:",
+        options: [
+            { id: 0, text: "плътност", isCorrect: false },
+            { id: 1, text: "маса", isCorrect: false },
+            { id: 2, text: "тегло", isCorrect: true },
+            { id: 3, text: "налягане", isCorrect: false },
+        ],
+    },
+    {
+        text: "Човек със ски потъва по-малко в снега, отколкото когато стъпва с обувки, защото:",
+        options: [
+            { id: 0, text: "натискът се увеличава", isCorrect: false },
+            { id: 1, text: "натискът намалява", isCorrect: false },
+            { id: 2, text: "натискът действа върху по-малка площ", isCorrect: false },
+            { id: 3, text: "натискът действа върху по-голяма площ", isCorrect: true },
+        ],
+    },
+    {
+        text: "За хидростатичното налягане е вярно, че:",
+        options: [
+            { id: 0, text: "намалява с увеличаване на дълбочината", isCorrect: false },
+            { id: 1, text: "е еднакво на всякаква дълбочина", isCorrect: false },
+            { id: 2, text: "на дъното на водния басейн е най-голямо", isCorrect: true },
+            { id: 3, text: "нараства с намаляване на дълбочината", isCorrect: false },
+        ],
+    },
+    {
+        text: "При наелектризиране на стъклена пръчка чрез триене с вълнен плат:",
+        options: [
+            { id: 0, text: "преминават протони от пръчката в плата", isCorrect: false },
+            { id: 1, text: "пръчката и вълненият плат получават еднакви по знак заряди", isCorrect: false },
+            { id: 2, text: "преминават електрони от пръчката във вълнения плат", isCorrect: true },
+            { id: 3, text: "се създават електрични заряди", isCorrect: false },
+        ],
+    },
+    {
+        text: "Електричният ток в металните проводници е насочено движение на:",
+        options: [
+            { id: 0, text: "протони", isCorrect: false },
+            { id: 1, text: "електрони", isCorrect: true },
+            { id: 2, text: "положителни йони", isCorrect: false },
+            { id: 3, text: "отрицателни йони", isCorrect: false },
+        ],
+    },
+    {
+        text: "Коя е липсващата дума или израз в изречението: Химичните елементи са съставни части на простите вещества и на ................... .",
+        options: [
+            { id: 0, text: "електроните", isCorrect: false },
+            { id: 1, text: "атомните ядра", isCorrect: false },
+            { id: 2, text: "протоните", isCorrect: false },
+            { id: 3, text: "химичните съединения", isCorrect: true },
+        ],
+    },
+    {
+        text: "Натриевият атом се превръща в положителен натриев йон като:",
+        options: [
+            { id: 0, text: "приеме един протон", isCorrect: false },
+            { id: 1, text: "отдаде един протон", isCorrect: false },
+            { id: 2, text: "приеме един електрон", isCorrect: false },
+            { id: 3, text: "отдаде един електрон", isCorrect: true },
+        ],
+    },
+    {
+        text: "Кой от посочените газове е основна причина за парниковия ефект?",
+        options: [
+            { id: 0, text: "въглероден оксид", isCorrect: false },
+            { id: 1, text: "серен диоксид", isCorrect: false },
+            { id: 2, text: "въглероден диоксид", isCorrect: true },
+            { id: 3, text: "азотни оксиди", isCorrect: false },
+        ],
+    },
+    {
+        text: "Кое от твърденията се отнася за химично свойство?",
+        options: [
+            { id: 0, text: "Медта лесно се кове и изтегля на нишки.", isCorrect: false },
+            { id: 1, text: "Желязото ръждясва в среда от влажен въздух.", isCorrect: true },
+            { id: 2, text: "Температурата на кипене на спирта е 78 о С.", isCorrect: false },
+            { id: 3, text: "Кислородът се разтваря слабо във вода.", isCorrect: false },
+        ],
+    },
+    {
+        text: "Кой от означните процеси е химично разлагане?",
+        options: [
+            { id: 0, text: "желязо + меден дихлорид -> железен дихлорид + мед", isCorrect: false },
+            { id: 1, text: "желязо + солна киселина -> водород и железен дихлорид", isCorrect: false },
+            { id: 2, text: "вода -> кислород + водород", isCorrect: true },
+            { id: 3, text: "желязо + сяра -> железен сулфид", isCorrect: false },
+        ],
+    },
+    {
+        text: "Като енергиен източник НЕ се използва:",
+        options: [
+            { id: 0, text: "течащата вода", isCorrect: false },
+            { id: 1, text: "слънцето", isCorrect: false },
+            { id: 2, text: "вятърът", isCorrect: false },
+            { id: 3, text: "желязната руда", isCorrect: true },
+        ],
+    },
+    {
+        text: "Коя от следните промени е химична?",
+        options: [
+            { id: 0, text: "разширяване на метално топче при нагряване", isCorrect: false },
+            { id: 1, text: "кондензация на водни пари", isCorrect: false },
+            { id: 2, text: "топене на сняг", isCorrect: false },
+            { id: 3, text: "горене на бензин", isCorrect: true },
+        ],
+    },
+    {
+        text: "В кой от отговорите са посочени САМО увреждания на опорно-двигателната система?",
+        options: [
+            { id: 0, text: "счупване и инфаркт", isCorrect: false },
+            { id: 1, text: "навяхване и изкълчване", isCorrect: true },
+            { id: 2, text: "рахит и инсулт", isCorrect: false },
+            { id: 3, text: "изкълчване и инсулт", isCorrect: false },
+        ],
+    },
+    {
+        text: "В кои органи кръвта отдава кислород и приема въглероден диоксид?",
+        options: [
+            { id: 0, text: "артериите", isCorrect: false },
+            { id: 1, text: "капилярите", isCorrect: true },
+            { id: 2, text: "белия дроб", isCorrect: false },
+            { id: 3, text: "вените", isCorrect: false },
+        ],
+    },
+    {
+        text: "За кой тип размножаване са необходими двама родители?",
+        options: [
+            { id: 0, text: "пъпкуване", isCorrect: false },
+            { id: 1, text: "безполово", isCorrect: false },
+            { id: 2, text: "вегетативно", isCorrect: false },
+            { id: 3, text: "полово", isCorrect: true },
+        ],
+    },
+    {
+        text: "Кое от заболяванията НЕ се предава по полов път?",
+        options: [
+            { id: 0, text: "бронхит", isCorrect: true },
+            { id: 1, text: "сифилис", isCorrect: false },
+            { id: 2, text: "гонорея", isCorrect: false },
+            { id: 3, text: "трихомониаза", isCorrect: false },
+        ],
+    },
 ];
 
 export default questionsChp;
