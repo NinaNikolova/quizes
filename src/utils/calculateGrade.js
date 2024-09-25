163755559806b0e1a41423649f85a4a900f980d3
nina <ninagbs@abv.bg>
2024-09-25 14:42:07 +0300

    added finish sounds

--- src/utils/calculateGrade.js
+++ src/utils/calculateGrade.js
@@ -1,13 +1,19 @@
+import { handleUserWin2, handleUserWin3, handleUserWin4, handleUserWin5, handleUserWin6 } from '../utils/sounds'
 export const calculateGrade = (percentage) => {
     if (percentage >= 90) {
+        handleUserWin6();
         return "Отличен(6) 🚀🥳👏";
     } else if (percentage >= 75) {
+        handleUserWin5();
         return "Много добър(5) 👏";
     } else if (percentage >= 60) {
+        handleUserWin4();
         return "Добър(4) 👌";
     } else if (percentage >= 45) {
+        handleUserWin3();
         return "Среден(3) 👀";
     } else {
+        handleUserWin2();
         return "Слаб(2)🙁";
     }
 };
--- src/utils/sounds.js
+++ src/utils/sounds.js
@@ -1,6 +1,13 @@
 import da from "../da.m4a";
 import ne from "../ne.m4a";
-import win from "../win.mp3";
+import win from "../win1.mp3";
+import two from "../two.m4a";
+import three from "../three.m4a";
+import four from "../four.m4a";
+import five from "../five.m4a";
+import six from "../six.m4a";
+
+
 
 export const handleUserInteractionDa = () => {
     const audio = new Audio(da);
@@ -19,6 +26,46 @@ export const handleUserInteractionNe = () => {
 
 export const handleUserWin = () => {
     const audio = new Audio(win);
+    audio.volume = 0.2;
+    audio.play().catch((error) => {
+        console.log('Playback prevented: ', error);
+    });
+
+};
+export const handleUserWin2 = () => {
+    const audio = new Audio(two);
+    audio.volume = 0.2;
+    audio.play().catch((error) => {
+        console.log('Playback prevented: ', error);
+    });
+
+};
+export const handleUserWin3 = () => {
+    const audio = new Audio(three);
+    audio.volume = 0.2;
+    audio.play().catch((error) => {
+        console.log('Playback prevented: ', error);
+    });
+
+};
+export const handleUserWin4 = () => {
+    const audio = new Audio(four);
+    audio.volume = 0.2;
+    audio.play().catch((error) => {
+        console.log('Playback prevented: ', error);
+    });
+
+};
+export const handleUserWin5 = () => {
+    const audio = new Audio(five);
+    audio.volume = 0.2;
+    audio.play().catch((error) => {
+        console.log('Playback prevented: ', error);
+    });
+
+};
+export const handleUserWin6 = () => {
+    const audio = new Audio(six);
     audio.volume = 0.2;
     audio.play().catch((error) => {
         console.log('Playback prevented: ', error);
